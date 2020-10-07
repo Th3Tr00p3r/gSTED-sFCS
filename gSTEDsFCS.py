76d2f3d40d17b6270a4e219123a8c0f27490db58
Ido Michealovich <40335982+Th3Tr00p3r@users.noreply.github.com>
2020-10-08 01:55:06 +0300

    Added clean_up_app (before quitting/restarting), exit_app and restart_app functions (added loop to gSTEDsFCS.py for restart capability)

--- gSTEDsFCS.py
+++ gSTEDsFCS.py
@@ -1,9 +1,14 @@
-from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication
 from ui.mainwindow import MainWindow
+import implementation.constants as const
 
 if __name__ == "__main__":
     import sys
-    app = QtWidgets.QApplication(sys.argv)
-    ui = MainWindow()
-    ui.show()
-    sys.exit(app.exec_())
+    current_exit_code = const.EXIT_CODE_REBOOT
+    while current_exit_code == const.EXIT_CODE_REBOOT:
+        app = QApplication(sys.argv)
+        ui = MainWindow()
+        ui.show()
+        current_exit_code = app.exec_()
+        app = None
+#        print(current_exit_code)
