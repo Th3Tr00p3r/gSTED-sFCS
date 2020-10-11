d13f414f2291867b054dd7e14445d1365b25963e
Ido Michealovich <40335982+Th3Tr00p3r@users.noreply.github.com>
2020-10-11 23:17:35 +0300

    changed ./ui to ./gui

--- gSTEDsFCS.py
+++ gSTEDsFCS.py
@@ -1,10 +1,17 @@
 from PyQt5.QtWidgets import QApplication
-from ui.mainwindow import MainWindow
+from gui.mainwindow import MainWindow
 import implementation.constants as const
 import icons
+import pyqt5ac
 
 if __name__ == "__main__":
     import sys
+    
+#    pyqt5ac.main(rccOptions='', uicOptions='--from-imports',
+#                       force=False, initPackage=True, config='',
+#                       ioPaths=[['ui/*.ui', 'generated/%%FILENAME%%_ui.py'],
+#                       ['resources/*.qrc', 'generated/%%FILENAME%%_rc.py']])
+    
     current_exit_code = const.EXIT_CODE_REBOOT
     while current_exit_code == const.EXIT_CODE_REBOOT:
         app = QApplication(sys.argv)
