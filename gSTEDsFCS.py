bbae30152d6fe9b7a9562561cade24eef13cc0f8
Lab PC <None>
2020-11-01 21:18:49 +0200

    added some initial log file handeling, tried to solve restart issue but can still only restart once

--- gSTEDsFCS.py
+++ gSTEDsFCS.py
@@ -2,13 +2,9 @@ from PyQt5.QtWidgets import QApplication
 from gui.gui import MainWindow
 import implementation.constants as const
 import gui.icons # NOQA
-#import pyqt5ac
 
 if __name__ == "__main__":
     import sys
-    
-#    pyqt5ac.main(rccOptions='', force=False, initPackage=True, config='',
-#                       ioPaths=['gui/icons/*.qrc', 'gui/icons/%%FILENAME%%_rc.py'])
     
     current_exit_code = const.EXIT_CODE_REBOOT
     while current_exit_code == const.EXIT_CODE_REBOOT:
@@ -16,5 +12,7 @@ if __name__ == "__main__":
         ui = MainWindow()
         ui.show()
         current_exit_code = app.exec_()
+        ui.destroy()
+        ui = None
         app = None
-#        print(current_exit_code)
+        print(current_exit_code)
