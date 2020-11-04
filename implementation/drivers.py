4de50a2ba2895dc3ac4aac0271d5228d27f89bf6
Lab PC <None>
2020-11-04 13:12:40 +0200

    made 'VISAInstrument' class to be a template for VISA devices, with added Task object for easy opening/closing. returned stage to previous configuration (it's a slow responding device)

--- implementation/drivers.py
+++ implementation/drivers.py
@@ -1,35 +1,99 @@
 import pyvisa as visa
 import nidaqmx
 from instrumental.drivers.cameras.uc480 import UC480_Camera as Camera # NOQA
+import implementation.logic as logic
 #import time #testing
+# TODO: add parent classes for general visa and daqmx control
 
+class VISAInstrument():
+    
+    def __init__(self, nick, address,
+                       read_termination='',
+                       write_termination=''):
+        
+        self.nick = nick
+        self.address = address
+        self.state = False
+        self.read_termination = read_termination
+        self.write_termination = write_termination
+        self.rm = visa.ResourceManager()
+    
+    class Task():
+        
+        def __init__(self, inst):
+            
+            self.inst = inst
+            
+    
+        def __enter__(self):
+            
+            self.rsrc = self.inst.rm.open_resource(self.inst.address,
+                                                    read_termination=self.inst.read_termination,
+                                                    write_termination=self.inst.write_termination)
+            return self.rsrc
+        
+        def __exit__(self, exc_type, exc_value, exc_tb):
+            
+            self.rsrc.close()
+    
 class ExcitationLaser():
     
     def __init__(self, nick, address):
         
         self.nick = nick
         self.address = address
-        self.comm_type = 'DO'
+        self.comm_type = 'DO' # to be used for a general definition of daqmx class
         # Turn OFF if somehow ON
         with nidaqmx.Task() as task:
             task.do_channels.add_do_chan(self.address)
             task.write(False)
-        self.state = 'OFF'
+        self.state = False
     
     def toggle(self):
         
         with nidaqmx.Task() as task:
             task.do_channels.add_do_chan(self.address)
-            task.write(self.state == 'OFF')
-        self.state = 'ON' if self.state == 'OFF' else 'OFF'
+            self.state = not self.state
+            task.write(self.state)
 
-class DepletionLaser():
+class DepletionLaser(VISAInstrument):
+    '''
+    Control depletion laser through pyVISA
+    '''
     
     def __init__(self, nick, address):
         
-        self.nick = nick
-        self.address = address
-        self.comm_type = None
+        self.SHG_temp = None
+        self.mode = None
+        self.current = None
+        self.power = None
+        super().__init__(nick=nick, address=address,
+                               read_termination = '\r', 
+                               write_termination = '\r')
+        with VISAInstrument.Task(self) as task:
+            task.write('setLDenable 0')
+    
+    def toggle(self):
+        
+        with VISAInstrument.Task(self) as task:
+    #        if self.SHG_temp > 52 :
+            if 1:
+                if not self.state:
+                    task.write('setLDenable 1')
+                else:
+                    task.write('setLDenable 0')
+            else:
+                logic.Error(error_txt='SHG temperature too low.')
+            self.state = not self.state
+        
+    def get_SHG_temp(self):
+        
+        rsrc = self.rm.open_resource(self.address)
+        rsrc.write('SHGtemp\r')
+        ans = rsrc.read_raw(size=1)
+        print(ans)
+        rsrc.close()
+        return ans
 
 class DepletionShutter():
     
@@ -42,15 +106,51 @@ class DepletionShutter():
         with nidaqmx.Task() as task:
             task.do_channels.add_do_chan(self.address)
             task.write(False)
-        self.state = 'CLOSED'
+        self.state = False
     
     def toggle(self):
         
         with nidaqmx.Task() as task:
             task.do_channels.add_do_chan(self.address)
-            task.write(self.state == 'CLOSED')
-        self.state = 'OPEN' if self.state == 'CLOSED' else 'CLOSED'
+            self.state = not self.state
+            task.write(self.state)
     
+class StepperStage():
+    '''
+    Control stepper stage through arduino chip using PyVISA.
+    This device operates slowly and needs special care.
+    '''
+    
+    def __init__(self, nick, address):
+        
+        self.nick = nick
+        self.address = address
+        self.rm = visa.ResourceManager()
+        self.state = False
+    
+    def open(self):
+        
+        self.rsrc = self.rm.open_resource(self.address)
+        self.state = True
+        
+    def close(self):
+        
+        self.rsrc.close()
+        self.state = False
+    
+    def move(self, dir=None,  steps=None):
+        
+        cmd_dict = {'UP': (lambda steps: 'my ' + str(-steps)),
+                          'DOWN': (lambda steps: 'my ' + str(steps)),
+                          'LEFT': (lambda steps: 'mx ' + str(steps)),
+                          'RIGHT': (lambda steps: 'mx ' + str(-steps))
+                        }
+        self.rsrc.write(cmd_dict[dir](steps))
+    
+    def release(self):
+
+        self.rsrc.write('ryx ')
+
         ####TESTING####
         
 ##        print('settings channel input: ',  chan)
@@ -67,33 +167,3 @@ class DepletionShutter():
 ##            print('digital lines: ',  device.di_lines.channel_names)
             
         ####TESTING####
-    
-    
-
-class StepperStage():
-    '''
-    Control stepper stage through pyVISA
-    '''
-    
-    def __init__(self, nick, address):
-        
-        self.nick = nick
-        self.address = address
-        self.rm = visa.ResourceManager()
-        self.state = 'OFF'
-    
-    def move(self, dir=None,  steps=None):
-        rsrc = self.rm.open_resource(self.address)
-        cmd_dict = {'UP': (lambda steps: 'my ' + str(-steps)),
-                          'DOWN': (lambda steps: 'my ' + str(steps)),
-                          'LEFT': (lambda steps: 'mx ' + str(steps)),
-                          'RIGHT': (lambda steps: 'mx ' + str(-steps))
-                        }
-        rsrc.write(cmd_dict[dir](steps))
-        rsrc.close()
-    
-    def release(self):
-        rsrc = self.rm.open_resource(self.address)
-        cmnd = 'ryx '
-        rsrc.write(cmnd)
-        rsrc.close()
