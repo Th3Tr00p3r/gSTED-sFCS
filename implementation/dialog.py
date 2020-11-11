d7843d31b9b0683e14093451acc5a8c08b0dcf2b
Lab PC <None>
2020-11-11 13:49:54 +0200

    moved log and dialogboxes from logic.py to seperate modules, other small improvements

---
+++ implementation/dialog.py
@@ -0,0 +1,58 @@
+import os
+import traceback
+import sys
+from PyQt5.QtWidgets import (QMessageBox)
+
+class UserDialog():
+    
+    def __init__(self,
+                       msg_icon=QMessageBox.NoIcon,
+                       msg_title=None,
+                       msg_text=None,
+                       msg_inf=None,
+                       msg_det=None):
+        self._msg_box = QMessageBox()
+        self._msg_box.setIcon(msg_icon)
+        self._msg_box.setWindowTitle(msg_title)
+        self._msg_box.setText(msg_text)
+        self._msg_box.setInformativeText(msg_inf)
+        self._msg_box.setDetailedText(msg_det)
+    
+    def set_buttons(self, std_bttn_list):
+        for std_bttn in std_bttn_list:
+            self._msg_box.addButton(getattr(QMessageBox, std_bttn))
+        
+    def display(self):
+        
+        return self._msg_box.exec_()
+    
+class Error(UserDialog):
+    
+    def __init__(self, exc=None, error_txt=''):
+        if exc:
+            self.exc_type, _, self.tb = sys.exc_info()
+            self.error_type = self.exc_type.__name__
+            self.fname = os.path.split(self.tb.tb_frame.f_code.co_filename)[1]
+            self.lineno = self.tb.tb_lineno
+            super().__init__(msg_icon=QMessageBox.Critical,
+                                   msg_title=self.error_type + ': ' + error_txt,
+                                   msg_text=str(exc),
+                                   msg_inf=('module: {}' +
+                                                '\n' + 'line: {}'
+                                                 ).format(self.fname, self.lineno),
+                                   msg_det='\n'.join(traceback.format_tb(self.tb))
+                                  )
+        else:
+            super().__init__(msg_icon=QMessageBox.Critical,
+                                   msg_title=str(exc),
+                                   msg_text=error_txt,
+                                  )
+
+class Question(UserDialog):
+    def __init__(self, q_txt,  q_title='User Input Needed'):
+        super().__init__(msg_icon=QMessageBox.Question,
+                               msg_title=q_title,
+                               msg_text=q_txt
+                               )
+        self.set_buttons(['Yes', 'No'])
+        self._msg_box.setDefaultButton(QMessageBox.No)
