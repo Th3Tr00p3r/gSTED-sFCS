a38e8c13332187dc73f89affc96535694330654a
Lab PC <None>
2020-11-18 14:54:42 +0200

    Physically switched depletion laser USB to COM6 (COM4 apparently made problems), and change default settings accordingly. Also made sure defualt settings are loaded before devices

--- implementation/error_handler.py
+++ implementation/error_handler.py
@@ -4,9 +4,10 @@ Error Handeling
 from implementation.dialog import Error
 from pyvisa.errors import VisaIOError
 from instrumental.drivers.cameras.uc480 import UC480Error
+from nidaqmx.errors import DaqError
 import functools
 
-def dvc_error_handler(func):
+def driver_error_handler(func):
     
     # TODO: possibly turn this into a class, and create subclasses as needed. also create one for logging
     @functools.wraps(func)
@@ -14,14 +15,25 @@ def dvc_error_handler(func):
         
         if not dvc.error_dict[dvc.nick]: # if there's no errors
             try:
-                func(dvc, *args, **kwargs)
+                return func(dvc, *args, **kwargs)
+            
+            except ValueError:
+                return -999
+            
+            except DaqError as exc:
+                Error(exc).display()
                 
             except VisaIOError as exc:
                 dvc.error_dict[dvc.nick] = 'VISA Error'
                 Error(exc, error_txt='VISA can\'t access ' + dvc.address +
                                                    ' (depletion laser)').display()
+            
+            except UC480Error as exc:
+                Error(exc).display()
+            
         else:
             print(F"'{dvc.nick}' error. Ignoring '{func.__name__}()' call.")
+            return -999
                                                
     return wrapper_error_handler
 
@@ -34,9 +46,6 @@ def logic_error_handler(func):
             func(*args, **kwargs)
             
         except FileNotFoundError as exc:
-            Error(exc).display()
-        
-        except UC480Error as exc:
             Error(exc).display()
                                                
     return wrapper_error_handler
