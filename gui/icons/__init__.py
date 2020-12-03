21d32a182fd5cf219fde4a50c11bd42199dfc514
Lab PC <None>
2020-12-03 14:12:43 +0200

    Now using 'pre-commit' to incorporate 'black' and 'flake8' for aggressive linting before commits. Many style changes made (only style)

--- gui/icons/__init__.py
+++ gui/icons/__init__.py
@@ -1,42 +1,57 @@
-'''
-documentation
-'''
+# -*- coding: utf-8 -*-
+""" documentation."""
+
 import glob
 import os
 
-def gen_icons_resource_file():
-    '''
-    documentation
-    '''
-    header = ('<!DOCTYPE RCC>' + '\n' +
-                  '<RCC version="1.0">' + '\n' +
-                  '<qresource>' + '\n')
-    
-    footer = ('</qresource>' + '\n' +
-                 '</RCC>' + '\n')
 
-    icon_paths = glob.glob('.\gui\icons\*.png')
-    
-    with open('./gui/icons/icons.qrc', 'w') as f: 
+def gen_icons_resource_file():
+    """documentation."""
+    header = (
+        "<!DOCTYPE RCC>"
+        + "\n"
+        + '<RCC version="1.0">'
+        + "\n"
+        + "<qresource>"
+        + "\n"
+    )
+    footer = "</qresource>" + "\n" + "</RCC>" + "\n"
+
+    icon_paths = glob.glob("./gui/icons/*.png")
+
+    with open("./gui/icons/icons.qrc", "w") as f:
         f.write(header)
         for icon_path in icon_paths:
             _, icon_fname = os.path.split(icon_path)
-            f.write('<file>' + icon_fname + '</file>' + '\n')
+            f.write("<file>" + icon_fname + "</file>" + "\n")
         f.write(footer)
 
+
 def gen_icon_paths_module():
-    header = ("'''" + '\n' +
-                  'Icon path constants' + '\n' +
-                  '(This is an automatically generated file, ' +
-                  'if needed, changes should be made in \'./icons/__init__.py)\'' + '\n'
-                  "'''" + '\n')
-    icon_paths = glob.glob('.\gui\icons\*.png')
-    with open('./gui/icons/icon_paths.py', 'w') as f: 
+    header = (
+        "# -*- coding: utf-8 -*-\n"
+        + '"""\n'
+        + "Icon path constants\n"
+        + "(This is an automatically generated file.\n"
+        + "if needed, changes should be made in"
+        + "'./icons/__init__.py)\n\n'"
+        + '"""\n\n'
+    )
+    icon_paths = glob.glob("./gui/icons/*.png")
+    with open("./gui/icons/icon_paths.py", "w") as f:
         f.write(header)
         for icon_path in icon_paths:
             _, icon_fname = os.path.split(icon_path)
             icon_fname_notype = os.path.splitext(icon_fname)[0]
-            f.write(icon_fname_notype.upper() + ' = \'' + icon_path + '\'' + '\n')
-            
+            f.write(
+                icon_fname_notype.upper()
+                + " = '"
+                + "./gui/icons/"
+                + icon_fname
+                + "'"
+                + "\n"
+            )
+
+
 gen_icons_resource_file()
 gen_icon_paths_module()
--- logic/measurements.py
+++ logic/measurements.py
@@ -1,17 +1,17 @@
-'''
-Measurements Module
-'''
+# -*- coding: utf-8 -*-
+"""Measurements Module."""
 
-class Measurement():
-    
+
+class Measurement:
+    """Doc."""
+
     # TODO: move timer to timeout_loop. this would mean all updates will be made from there too (progress bar etc.)
-    
-    def __init__(self, app, type=None,
-                       duration_spinner=None,
-                       prog_bar=None,
-                       log=None):
-                           
-        # instance attributes
+
+    def __init__(
+        self, app, type=None, duration_spinner=None, prog_bar=None, log=None
+    ):
+        """Doc."""
+
         self._app = app
         self.type = type
         self.duration_spinner = duration_spinner
@@ -19,25 +19,30 @@ class Measurement():
         self.log = log
         self.time_passed = 0
         self.update_ready = False
-        
+
     def start(self):
-        
-        self._app.dvc_dict['TDC'].toggle(True)
-        self.log.update(F"{self.type} measurement started")
-        
+        """Doc."""
+
+        self._app.dvc_dict["TDC"].toggle(True)
+        self.log.update(f"{self.type} measurement started")
+
     def stop(self):
-        
+        """Doc."""
+
         self.type = None
-        
-        self._app.dvc_dict['TDC'].toggle(False)
-            
+
+        self._app.dvc_dict["TDC"].toggle(False)
+
         if self.prog_bar:
             self.prog_bar.setValue(0)
-            
-        self.log.update(F"{self.type} measurement stopped")
-    
+
+        self.log.update(f"{self.type} measurement stopped")
+
     def disp_ACF(self):
-        
-        print(F"Measurement Finished:\n"
-                F"Full Data = {self._app.dvc_dict['UM232'].data}\n"
-                F"Total Bytes = {self._app.dvc_dict['UM232'].tot_bytes}\n")
+        """Doc."""
+
+        print(
+            f"Measurement Finished:\n"
+            f"Full Data = {self._app.dvc_dict['UM232'].data}\n"
+            f"Total Bytes = {self._app.dvc_dict['UM232'].tot_bytes}\n"
+        )
