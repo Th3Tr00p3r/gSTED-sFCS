721086c7cd89f761145cd5ba6721f64025c3dfde
Lab PC <None>
2021-03-08 11:32:18 +0200

    Angular scan finally operational, next is circular scan

--- logic/measurements.py
+++ logic/measurements.py
@@ -7,7 +7,7 @@ import logging
 import math
 import time
 from types import SimpleNamespace
-from typing import NoReturn, Tuple, Union
+from typing import NoReturn, Tuple
 
 import nidaqmx.constants as ni_consts  # TODO: move to consts.py
 import numpy as np
@@ -25,10 +25,17 @@ class Measurement:
 
         self._app = app
         self.type = type
-        self.scan_params = SimpleNamespace()
-        [setattr(self.scan_params, key, val) for key, val in scan_params.items()]
-        [setattr(self, key, val) for key, val in kwargs.items()]
         self.data_dvc = app.devices.UM232H
+        [setattr(self, key, val) for key, val in kwargs.items()]
+        if scan_params:
+            self.pxl_clk_dvc = app.devices.PXL_CLK
+            self.scanners_dvc = app.devices.SCANNERS
+            self.counter_dvc = app.devices.COUNTER
+            self.scan_params = SimpleNamespace()
+            [setattr(self.scan_params, key, val) for key, val in scan_params.items()]
+            self.um_V_ratio = tuple(
+                getattr(self.scanners_dvc, f"{ax.lower()}_um2V_const") for ax in "XYZ"
+            )
         self.is_running = False
 
     async def start(self):
@@ -73,25 +80,76 @@ class Measurement:
             laser_config = "nolaser"
         return laser_config
 
+    def init_scan_tasks(self, ao_sample_mode: str):
+        """Doc."""
+
+        if ao_sample_mode == "FINITE":
+            ao_sample_mode = ni_consts.AcquisitionType.FINITE
+        elif ao_sample_mode == "CONTINUOUS":
+            ao_sample_mode = ni_consts.AcquisitionType.CONTINUOUS
+
+        self.scanners_dvc.start_write_task(
+            ao_data=self.ao_buffer,
+            type=self.scan_params.scan_type,
+            samp_clk_cnfg_xy={
+                "source": self.pxl_clk_dvc.out_term,
+                "sample_mode": ao_sample_mode,
+                "samps_per_chan": self.n_ao_samps,
+                "rate": 100000,
+            },
+            samp_clk_cnfg_z={
+                "source": self.pxl_clk_dvc.out_ext_term,
+                "sample_mode": ao_sample_mode,
+                "samps_per_chan": self.n_ao_samps,
+                "rate": 100000,
+            },
+        )
+
+        self.scanners_dvc.start_scan_read_task(
+            samp_clk_cnfg={},
+            timing_params={
+                "samp_quant_samp_mode": ni_consts.AcquisitionType.CONTINUOUS,
+                "samp_quant_samp_per_chan": int(self.n_ao_samps * 1.2),
+                "samp_timing_type": ni_consts.SampleTimingType.SAMPLE_CLOCK,
+                "samp_clk_src": self.scanners_dvc.tasks.ao[
+                    "AO XY"
+                ].timing.samp_clk_term,
+                "ai_conv_rate": self.ai_conv_rate,
+            },
+        )
+
+        self.counter_dvc.start_scan_read_task(
+            samp_clk_cnfg={},
+            timing_params={
+                "samp_quant_samp_mode": ni_consts.AcquisitionType.CONTINUOUS,
+                "samp_quant_samp_per_chan": int(self.n_ao_samps * 1.2),
+                "samp_timing_type": ni_consts.SampleTimingType.SAMPLE_CLOCK,
+                "samp_clk_src": self.scanners_dvc.tasks.ao[
+                    "AO XY"
+                ].timing.samp_clk_term,
+            },
+        )
+
+    def return_to_regular_tasks(self):
+        """Close AO tasks and resume continuous AI/CI"""
+
+        self.scanners_dvc.stop_write_task()
+        self.scanners_dvc.start_continuous_read_task()
+        self.counter_dvc.start_continuous_read_task()
+
 
 class SFCSImageMeasurement(Measurement):
     """Doc."""
 
     def __init__(self, app, scan_params, **kwargs):
         super().__init__(app=app, type="SFCSImage", scan_params=scan_params, **kwargs)
-        self.pxl_clk_dvc = app.devices.PXL_CLK
-        self.scanners_dvc = app.devices.SCANNERS
-        self.counter_dvc = app.devices.COUNTER
         self.laser_dvcs = SimpleNamespace()
         self.laser_dvcs.exc = app.devices.EXC_LASER
         self.laser_dvcs.dep = app.devices.DEP_LASER
         self.laser_dvcs.dep_shutter = app.devices.DEP_SHUTTER
-        self.um_V_ratio = tuple(
-            getattr(self.scanners_dvc, f"{ax}_um2V_const") for ax in "xyz"
-        )
 
     def build_filename(self, file_no: int) -> str:
-        return f"{self.file_template}_{self.laser_config}_{self.scan_params.scn_type}_{file_no}"
+        return f"{self.file_template}_{self.laser_config}_{self.scan_params.scan_type}_{file_no}"
 
     async def toggle_lasers(self, finish=False) -> NoReturn:
         """Doc."""
@@ -138,14 +196,14 @@ class SFCSImageMeasurement(Measurement):
             else:
                 return div_ceil(ppl, 2) * 2
 
-        def bld_scn_addrs_str(scn_type: str, scanners_dvc) -> str:
+        def bld_scn_addrs_str(scan_type: str, scanners_dvc) -> str:
             """Doc."""
 
-            if scn_type == "XY":
+            if scan_type == "XY":
                 return ", ".join([scanners_dvc.ao_x_addr, scanners_dvc.ao_y_addr])
-            elif scn_type == "YZ":
+            elif scan_type == "YZ":
                 return ", ".join([scanners_dvc.ao_y_addr, scanners_dvc.ao_z_addr])
-            elif scn_type == "XZ":
+            elif scan_type == "XZ":
                 return ", ".join([scanners_dvc.ao_x_addr, scanners_dvc.ao_z_addr])
 
         # fix line freq, pxl clk low ticks, and ppl
@@ -161,92 +219,36 @@ class SFCSImageMeasurement(Measurement):
             self.scan_params.ppl,
         )
         # unite relevant physical addresses
-        self.scn_addrs = bld_scn_addrs_str(self.scan_params.scn_type, self.scanners_dvc)
+        # TODO: is prop in next line ever used? if not, should delete it and the related function
+        self.scn_addrs = bld_scn_addrs_str(
+            self.scan_params.scan_type, self.scanners_dvc
+        )
         # init device buffers
         self.scanners_dvc.init_ai_buffer()
         self.counter_dvc.init_ci_buffer()
         # create ao_buffer
         (
             self.ao_buffer,
-            self.dt,
+            dt,
             self.set_pnts_lines_odd,
             self.set_pnts_lines_even,
             self.set_pnts_planes,
-            self.total_pnts,
         ) = ScanPatternAO("image", self.scan_params, self.um_V_ratio).calculate_ao()
-        # TODO: why is the next line correct? explain and use a constant for 1.5E-7
-        # TODO: create a scan arguments/parameters object to send to scanners_dvc (or simply for more clarity)
         self.n_ao_samps = len(self.ao_buffer[0])
-        self.ai_conv_rate = 1 / (
-            (self.dt - 1.5e-7) / self.scanners_dvc.ai_buffer.shape[0]
-        )
-
-    def init_scan_tasks(self):
-        """Doc."""
-
-        self.scanners_dvc.start_write_task(
-            ao_data=self.ao_buffer,
-            type=self.scan_params.scn_type,
-            samp_clk_cnfg_xy={
-                "source": self.pxl_clk_dvc.out_term,
-                "sample_mode": ni_consts.AcquisitionType.FINITE,
-                "samps_per_chan": self.n_ao_samps,
-                "rate": 100000,
-            },
-            samp_clk_cnfg_z={
-                "source": self.pxl_clk_dvc.out_ext_term,
-                "sample_mode": ni_consts.AcquisitionType.FINITE,
-                "samps_per_chan": self.n_ao_samps,
-                "rate": 100000,
-            },
-        )
-
-        self.scanners_dvc.start_scan_read_task(
-            samp_clk_cnfg={},
-            timing_params={
-                "samp_quant_samp_mode": ni_consts.AcquisitionType.CONTINUOUS,
-                "samp_quant_samp_per_chan": int(self.total_pnts * 1.2),
-                "samp_timing_type": ni_consts.SampleTimingType.SAMPLE_CLOCK,
-                "samp_clk_src": self.scanners_dvc.tasks.ao[
-                    "AO XY"
-                ].timing.samp_clk_term,
-                "ai_conv_rate": self.ai_conv_rate,
-            },
-        )
-
-        self.counter_dvc.start_scan_read_task(
-            samp_clk_cnfg={},
-            timing_params={
-                "samp_quant_samp_mode": ni_consts.AcquisitionType.CONTINUOUS,
-                "samp_quant_samp_per_chan": int(self.total_pnts * 1.2),
-                "samp_timing_type": ni_consts.SampleTimingType.SAMPLE_CLOCK,
-                "samp_clk_src": self.scanners_dvc.tasks.ao[
-                    "AO XY"
-                ].timing.samp_clk_term,
-            },
-        )
+        # TODO: why is the next line correct? explain and use a constant for 1.5E-7
+        self.ai_conv_rate = 1 / ((dt - 1.5e-7) / self.scanners_dvc.ai_buffer.shape[0])
 
     def change_plane(self, plane_idx):
         """Doc."""
 
         for axis in "XYZ":
-            if axis not in self.scan_params.scn_type:
+            if axis not in self.scan_params.scan_type:
                 plane_axis = axis
                 break
 
         self.scanners_dvc.start_write_task(
             ao_data=[[self.set_pnts_planes[plane_idx]]],
             type=plane_axis,
-        )
-
-    def move_to_start(self):
-        """Doc."""
-        # TODO: perhaps move back to current AO when done?
-
-        self.scanners_dvc.start_write_task(
-            # TODO: clearer way do get the following line?
-            ao_data=[[self.ao_buffer[0][0]], [self.ao_buffer[1][0]]],
-            type=self.scan_params.scn_type,
         )
 
     async def run(self):
@@ -256,7 +258,6 @@ class SFCSImageMeasurement(Measurement):
         await self.toggle_lasers()
         self.setup_scan()
         self._app.gui.main.imp.dvc_toggle("PXL_CLK")
-        self.move_to_start()
 
         plane_data = []
         logging.info(f"Running {self.type} measurement")
@@ -271,7 +272,7 @@ class SFCSImageMeasurement(Measurement):
 
                 self.data_dvc.purge()
 
-                self.init_scan_tasks()
+                self.init_scan_tasks("FINITE")
 
                 await asyncio.to_thread(self.data_dvc.ao_task_sync_read_TDC, self)
 
@@ -293,14 +294,12 @@ class SFCSImageMeasurement(Measurement):
         #        filename = self.build_filename()
         #        self.save_data(filename)
 
-        self._app.gui.main.numPlaneShown.setValue(
-            1
-        )  # TODO: show middle plane? or lowest?
+        # TODO: show middle plane? or lowest? add gui object to params
+        self._app.gui.main.numPlaneShown.setValue(1)
 
         self._app.gui.main.imp.dvc_toggle("PXL_CLK")
         await self.toggle_lasers(finish=True)
-        self.scanners_dvc.start_continuous_read_task()
-        self.counter_dvc.start_continuous_read_task()
+        self.return_to_regular_tasks()
 
         if self.is_running:  # if not manually stopped
             self._app.gui.main.imp.toggle_meas(self.type)
@@ -313,50 +312,30 @@ class SFCSSolutionMeasurement(Measurement):
         super().__init__(
             app=app, type="SFCSSolution", scan_params=scan_params, **kwargs
         )
+        self.scan_params.scan_type = "XY"
         self.duration_multiplier = 60
         self.laser_config = self.get_laser_config()
 
     def build_filename(self, file_no: int) -> str:
         return f"{self.file_template}_{self.laser_config}_{file_no}"
 
-    async def run(self):
+    def get_current_and_end_times(self) -> Tuple[datetime.time, datetime.time]:
+        """
+        Given a duration in seconds, returns a tuple (current_time, end_time)
+        in datetime.time format, where end_time is current_time + duration_in_seconds.
+        """
+
+        duration_in_seconds = int(self.total_duration * self.duration_multiplier)
+        curr_datetime = datetime.datetime.now()
+        curr_time = datetime.datetime.now().time()
+        end_time = (
+            curr_datetime + datetime.timedelta(seconds=duration_in_seconds)
+        ).time()
+        return curr_time, end_time
+
+    async def calibrate_num_files(self):
         """Doc."""
-        # TODO: this needs refactoring - add more functions and clean up code
-        # TODO: Add check if file templeate exists in save dir, and if so confirm overwrite or cancel
 
-        def get_current_and_end_times(
-            duration_in_seconds: Union[int, float]
-        ) -> Tuple[datetime.time, datetime.time]:
-            """
-            Given a duration in seconds, returns a tuple (current_time, end_time)
-            in datetime.time format, where end_time is current_time + duration_in_seconds.
-            """
-
-            duration_in_seconds = int(duration_in_seconds)
-            curr_datetime = datetime.datetime.now()
-            curr_time = datetime.datetime.now().time()
-            end_time = (
-                curr_datetime + datetime.timedelta(seconds=duration_in_seconds)
-            ).time()
-            return (curr_time, end_time)
-
-        def disp_ACF(data_dvc):
-            """Placeholder for calculating and presenting the ACF"""
-
-            print(
-                f"Measurement Finished:\n"
-                f"Full Data[:100] = {data_dvc.data[:100]}\n"
-                f"Total Bytes = {data_dvc.tot_bytes_read}\n"
-            )
-
-        # initialize gui start/end times
-        start_time, end_time = get_current_and_end_times(
-            self.total_duration * self.duration_multiplier
-        )
-        self.start_time_wdgt.set(start_time)
-        self.end_time_wdgt.set(end_time)
-
-        # calibrating save-intervals
         saved_dur_mul = self.duration_multiplier
         self.duration = self.cal_duration
         self.duration_multiplier = 1  # in seconds
@@ -386,6 +365,63 @@ class SFCSSolutionMeasurement(Measurement):
         # determining number of files
         num_files = int(math.ceil(self.total_duration / self.save_intrvl))
         self.total_files_wdgt.set(num_files)
+        return num_files
+
+    def setup_scan(self):
+        """Doc."""
+
+        # make sure divisablility, then sync pixel clock with AO sample clock
+        if (self.pxl_clk_dvc.freq_MHz * 1e6) % self.scan_params.ao_samp_freq_Hz != 0:
+            raise ValueError(
+                f"Pixel clock ({self.pxl_clk_dvc.freq_MHz} Hz) and AO samplng ({self.scan_params.ao_samp_freq_Hz} Hz) frequencies aren't divisible."
+            )
+        else:
+            self.pxl_clk_dvc.low_ticks = (
+                int(
+                    (self.pxl_clk_dvc.freq_MHz * 1e6) / self.scan_params.ao_samp_freq_Hz
+                )
+                - 2
+            )
+
+        # init device buffers
+        self.scanners_dvc.init_ai_buffer()
+        self.counter_dvc.init_ci_buffer()
+
+        # create ao_buffer
+        (self.ao_buffer, dt, self.scan_params) = ScanPatternAO(
+            "angular", self.scan_params, self.um_V_ratio
+        ).calculate_ao()
+        self.n_ao_samps = len(self.ao_buffer[0])
+        # TODO: ask Oleg: why is the next line correct? explain and use a constant for 1.5E-7
+        self.ai_conv_rate = 1 / ((dt - 1.5e-7) / self.scanners_dvc.ai_buffer.shape[0])
+
+    def disp_ACF(self):
+        """Placeholder for calculating and presenting the ACF"""
+
+        print(
+            f"Measurement Finished:\n"
+            f"Full Data[:100] = {self.data_dvc.data[:100]}\n"
+            f"Total Bytes = {self.data_dvc.tot_bytes_read}\n"
+        )
+
+    async def run(self):
+        """Doc."""
+        # TODO: this needs refactoring - add more functions and clean up code
+        # TODO: Add check if file templeate exists in save dir, and if so confirm overwrite or cancel
+
+        # initialize gui start/end times
+        start_time, end_time = self.get_current_and_end_times()
+        self.start_time_wdgt.set(start_time)
+        self.end_time_wdgt.set(end_time)
+
+        # calibrating save-intervals/num_files
+        # TODO: test if non-scanning calibration gives good enough approximation for file size, if not, consider scanning inside cal function
+        num_files = await self.calibrate_num_files()
+
+        self.setup_scan()
+        self._app.gui.main.imp.dvc_toggle("PXL_CLK")
+        self.data_dvc.purge()
+        self.init_scan_tasks("CONTINUOUS")  # TODO: TEST
 
         self.total_time_passed = 0
         logging.info(f"Running {self.type} measurement")
@@ -402,7 +438,7 @@ class SFCSSolutionMeasurement(Measurement):
 
                 self.total_time_passed += self.time_passed
 
-                disp_ACF(self.data_dvc)
+                self.disp_ACF()
 
                 file_name = self.build_filename(file_num)
                 self.save_data(file_name)
@@ -411,6 +447,9 @@ class SFCSSolutionMeasurement(Measurement):
 
             else:
                 break
+
+        self._app.gui.main.imp.dvc_toggle("PXL_CLK")
+        self.return_to_regular_tasks()
 
         if self.is_running:  # if not manually stopped
             self._app.gui.main.imp.toggle_meas(self.type)
