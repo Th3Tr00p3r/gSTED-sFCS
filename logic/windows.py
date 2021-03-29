bc58bbd09a01710453896a13c7734bf038a9f42b
Ido Michealovich <40335982+Th3Tr00p3r@users.noreply.github.com>
2021-03-29 13:08:48 +0300

    Refactoring - changed QWidgetCollection to create the WidgetAccess object by itself, changed init_devices() in app.py as well as related functions/constants so that now 'param_dict' contains all the attributes needed for the device (no longer using DeviceAttr class after init)

--- logic/windows.py
+++ logic/windows.py
@@ -85,7 +85,6 @@ class MainWin:
         """Doc."""
 
         dvc = getattr(self._app.devices, nick)
-        DVC_CONSTS = getattr(consts, nick)
 
         if (leave_on and dvc.state is True) or (leave_off and dvc.state is False):
             return
@@ -94,13 +93,13 @@ class MainWin:
             dvc.toggle(True)
 
             if dvc.state:  # if managed to turn ON
-                if DVC_CONSTS.switch_widget is not None:
-                    DVC_CONSTS.switch_widget.set(QIcon(icon.SWITCH_ON))
+                if dvc.switch_widget is not None:
+                    dvc.switch_widget.set(QIcon(icon.SWITCH_ON))
 
-                on_icon = QIcon(DVC_CONSTS.led_icon_path)
-                DVC_CONSTS.led_widget.set(on_icon)
+                on_icon = QIcon(dvc.led_icon_path)
+                dvc.led_widget.set(on_icon)
 
-                logging.debug(f"{DVC_CONSTS.log_ref} toggled ON")
+                logging.debug(f"{dvc.log_ref} toggled ON")
 
                 if nick == "STAGE":
                     self._gui.stageButtonsGroup.setEnabled(True)
@@ -109,12 +108,12 @@ class MainWin:
             dvc.toggle(False)
 
             if not dvc.state:  # if managed to turn OFF
-                if DVC_CONSTS.switch_widget is not None:
-                    DVC_CONSTS.switch_widget.set(QIcon(icon.SWITCH_OFF))
+                if dvc.switch_widget is not None:
+                    dvc.switch_widget.set(QIcon(icon.SWITCH_OFF))
 
-                DVC_CONSTS.led_widget.set(QIcon(icon.LED_OFF))
+                dvc.led_widget.set(QIcon(icon.LED_OFF))
 
-                logging.debug(f"{DVC_CONSTS.log_ref} toggled OFF")
+                logging.debug(f"{dvc.log_ref} toggled OFF")
 
                 if nick == "STAGE":
                     self._gui.stageButtonsGroup.setEnabled(False)
@@ -127,16 +126,8 @@ class MainWin:
     def led_clicked(self, led_obj_name):
         """Doc."""
 
-        def dvc_nick_from_led_name(led_obj_name) -> str:
-            """Doc."""
-
-            led_to_nick_dict = {
-                getattr(consts, dvc_nick).led_widget.obj_name: dvc_nick
-                for dvc_nick in consts.DVC_NICKS_TUPLE
-            }
-            return led_to_nick_dict[led_obj_name]
-
-        dvc_nick = dvc_nick_from_led_name(led_obj_name)
+        LED_NAME_DVC_NICK_DICT = helper.inv_dict(consts.DVC_NICK_LED_NAME_DICT)
+        dvc_nick = LED_NAME_DVC_NICK_DICT[led_obj_name]
         err_dict = getattr(self._app.devices, dvc_nick).error_dict
         if err_dict is not None:
             Error(**err_dict, custom_title=getattr(consts, dvc_nick).log_ref).display()
