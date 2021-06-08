1ec32b4b08c5917bbcd6ddec62e7c0147fe55b51
Lab PC <None>
2021-06-08 12:39:18 +0300

    updated gitignore

---
+++ gui/icons/icon_paths.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Icon path constants
+(This is an automatically generated file.
+if needed, changes should be made in'./icons/__init__.py)
+
+'"""
+
+CAMERA = "./gui/icons/camera.png"
+CROSSHAIR = "./gui/icons/crosshair.png"
+LASER_BLUE = "./gui/icons/laser_blue.png"
+LED_BLUE = "./gui/icons/led_blue.png"
+LED_GREEN = "./gui/icons/led_green.png"
+LED_OFF = "./gui/icons/led_off.png"
+LED_ORANGE = "./gui/icons/led_orange.png"
+LED_RED = "./gui/icons/led_red.png"
+LOAD = "./gui/icons/load.png"
+LOG = "./gui/icons/log.png"
+PREFERENCES = "./gui/icons/preferences.png"
+RESTART = "./gui/icons/restart.png"
+SAVE_AS = "./gui/icons/save_as.png"
+SETTINGS = "./gui/icons/settings.png"
+STAGE = "./gui/icons/stage.png"
+SWITCH_OFF = "./gui/icons/switch_off.png"
+SWITCH_ON = "./gui/icons/switch_on.png"
+ZOOM = "./gui/icons/zoom.png"
