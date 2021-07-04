858bc63e96256e873b4f653e36d05ee3cd326fe2
Lab PC <None>
2021-07-04 15:44:55 +0300

    Revert "refactoring - converted single-use private methods into local functions for better readability. got rid of displayed messages ('another currently running...') during measurements, since those are blocking and require application restart after closing"
    This reverts commit 858d9233e61c3ea27e6d976082227a85885a0ea3.

--- logic/timeout.py
+++ logic/timeout.py
@@ -40,251 +40,247 @@ class Timeout:
         self.cntr_dvc = self._app.devices.photon_detector
         self.dep_dvc = self._app.devices.dep_laser
 
-        # start
-        self.not_finished = True
-        logging.debug("Initiating _timeout function.")
-        self._app.loop.create_task(self._timeout())
-
     # MAIN
-    async def _timeout(self) -> None:
+    async def timeout(self) -> None:
         """awaits all individual async loops, each with its own repeat time."""
 
-        # start the loops
         await asyncio.gather(
-            update_CI_and_AI(self),
-            update_avg_counts(self),
-            update_dep(self),
-            update_application_log(self),
-            update_gui(self),
-            # self._update_um232h_status(self), # TODO: this seems to cause an issue during measurements (noticed in solution scan) - try to see if it does and catch the error
+            self._updt_CI_and_AI(),
+            self._update_avg_counts(),
+            self._update_dep(),
+            self._updt_application_log(),
+            self._update_gui(),
+            # self._updt_um232h_status(), # TODO: this seems to cause an issue during measurements (noticed in solution scan) - try to see if it does and catch the error
         )
 
-        logging.debug("_timeout function exited")
+        logging.debug("timeout function exited")
 
-
-async def update_application_log(master) -> None:
-    """Doc."""
-
-    def get_last_line(file_path) -> str:
-        """
-        Return the last line of a text file.
-        (https://stackoverflow.com/questions/46258499/read-the-last-line-of-a-file-in-python)
-        """
-
-        try:
-            with open(file_path, "rb") as f:
-                f.seek(-2, os.SEEK_END)
-                while f.read(1) != b"\n":
-                    f.seek(-2, os.SEEK_CUR)
-                last_line = f.readline().decode()
-        except FileNotFoundError:
-            return "Log File Not Found!!!"
-        else:
-            return last_line
-
-    while master.not_finished:
-
-        new_maxlen = master._app.gui.main.logNumLinesSlider.value()
-        if master.log_buffer_deque.maxlen != new_maxlen:
-            master.log_buffer_deque = deque(master.log_buffer_deque, maxlen=new_maxlen)
-
-        last_line = get_last_line(LOG_PATH)
-
-        if last_line.find("INFO") != -1:
-            line_time = last_line[12:23]
-            line_text = last_line[38:]
-            last_line = line_time + line_text
-
-            if last_line != master.log_buffer_deque[0]:
-                master.log_buffer_deque.appendleft(last_line)
-
-                text = "".join(master.log_buffer_deque)
-                master._app.gui.main.lastAction.setPlainText(text)
-
-        await asyncio.sleep(0.3)
-
-
-async def update_CI_and_AI(master) -> None:
-    """Doc."""
-
-    #        N_TOC = 100 # TESTING
-    #        tocs = np.zeros(shape=(N_TOC,)) # TESTING
-    #        toc_idx = 0 # TESTING
-    #        tic = time.perf_counter() # TESTING
-
-    while master.not_finished:
-
-        #            if toc_idx == N_TOC: # TESTING
-        #                print(f"average timeout elapsed: {tocs.mean()}. Should be: {TIMEOUT}") # TESTING
-        #                toc_idx = 0 # TESTING
-        #            tocs[toc_idx] = time.perf_counter() - tic # TESTING
-        #            toc_idx += 1 # TESTING
-
-        # photon_detector
-        if not master._app.devices.photon_detector.error_dict:
-            master._app.devices.photon_detector.fill_ci_buffer()
-            if master._app.meas.type not in {"SFCSImage"}:
-                master._app.devices.photon_detector.dump_ci_buff_overflow()
-
-        # AI
-        if not master._app.devices.scanners.error_dict:
-            master._app.devices.scanners.fill_ai_buffer()
-            if master._app.meas.type not in {"SFCSSolution", "SFCSImage"}:
-                master._app.devices.scanners.dump_ai_buff_overflow()
-
-        #            tic = time.perf_counter() # TESTING
-
-        await asyncio.sleep(TIMEOUT)
-
-
-async def update_gui(master) -> None:
-    """Doc."""
-
-    def update_scn_pos(app):
+    def start(self) -> None:
         """Doc."""
 
-        try:
-            (
-                x_ai,
-                y_ai,
-                z_ai,
-                x_ao_int,
-                y_ao_int,
-                z_ao_int,
-            ) = app.devices.scanners.ai_buffer[:, -1]
+        self.not_finished = True
+        self._app.loop.create_task(self.timeout())
+        logging.debug("Initiating timeout function.")
 
-        except IndexError:
-            # AI buffer has just been initialized
-            pass
-
-        else:
-            (x_um, y_um, z_um) = tuple(
-                (axis_vltg - axis_org) * axis_ratio
-                for axis_vltg, axis_ratio, axis_org in zip(
-                    (x_ao_int, y_ao_int, z_ao_int),
-                    app.devices.scanners.um_v_ratio,
-                    app.devices.scanners.ORIGIN,
-                )
-            )
-
-            app.gui.main.xAIV.setValue(x_ai)
-            app.gui.main.yAIV.setValue(y_ai)
-            app.gui.main.zAIV.setValue(z_ai)
-
-            app.gui.main.xAOVint.setValue(x_ao_int)
-            app.gui.main.yAOVint.setValue(y_ao_int)
-            app.gui.main.zAOVint.setValue(z_ao_int)
-
-            app.gui.main.xAOum.setValue(x_um)
-            app.gui.main.yAOum.setValue(y_um)
-            app.gui.main.zAOum.setValue(z_um)
-
-    def update_meas_progbar(meas) -> None:
+    async def _updt_application_log(self) -> None:
         """Doc."""
 
-        try:
-            if meas.type == "SFCSSolution":
-                if not meas.cal:
-                    progress = (
-                        (meas.total_time_passed + meas.time_passed)
-                        / (meas.total_duration * meas.duration_multiplier)
-                        * meas.prog_bar_wdgt.obj.maximum()
-                    )
-                else:
-                    progress = 0
-            elif meas.type == "SFCSImage":
-                progress = (
-                    meas.time_passed / meas.est_total_duration * meas.prog_bar_wdgt.obj.maximum()
-                )
-            meas.prog_bar_wdgt.set(progress)
+        while self.not_finished:
 
-        except AttributeError:
-            # happens when depletion is turned on before beginning measurement (5 s wait)
-            pass
+            new_maxlen = self._app.gui.main.logNumLinesSlider.value()
+            if self.log_buffer_deque.maxlen != new_maxlen:
+                self.log_buffer_deque = deque(self.log_buffer_deque, maxlen=new_maxlen)
 
-        except Exception as exc:
-            err_hndlr(exc, locals(), sys._getframe())
+            last_line = get_last_line(LOG_PATH)
 
-    while master.not_finished:
+            if last_line.find("INFO") != -1:
+                line_time = last_line[12:23]
+                line_text = last_line[38:]
+                last_line = line_time + line_text
 
-        # scanners
-        update_scn_pos(master._app)
+                if last_line != self.log_buffer_deque[0]:
+                    self.log_buffer_deque.appendleft(last_line)
 
-        # Measurement progress bar
-        if master._app.meas.is_running:
-            update_meas_progbar(master._app.meas)
+                    text = "".join(self.log_buffer_deque)
+                    self._app.gui.main.lastAction.setPlainText(text)
 
-        await asyncio.sleep(master.updt_intrvl["gui"])
+            await asyncio.sleep(0.3)
 
+    async def _updt_CI_and_AI(self) -> None:
+        """Doc."""
 
-async def update_um232h_status(master):
-    """Doc."""
+        #        N_TOC = 100 # TESTING
+        #        tocs = np.zeros(shape=(N_TOC,)) # TESTING
+        #        toc_idx = 0 # TESTING
+        #        tic = time.perf_counter() # TESTING
 
-    while master.not_finished:
+        while self.not_finished:
 
-        try:
-            rx_bytes = master._app.devices.UM232H.get_status()
+            #            if toc_idx == N_TOC: # TESTING
+            #                print(f"average timeout elapsed: {tocs.mean()}. Should be: {TIMEOUT}") # TESTING
+            #                toc_idx = 0 # TESTING
+            #            tocs[toc_idx] = time.perf_counter() - tic # TESTING
+            #            toc_idx += 1 # TESTING
 
-        except DeviceError:
-            pass
+            # photon_detector
+            if not self._app.devices.photon_detector.error_dict:
+                self._app.devices.photon_detector.fill_ci_buffer()
+                if self._app.meas.type not in {"SFCSImage"}:
+                    self._app.devices.photon_detector.dump_ci_buff_overflow()
 
-        else:
-            fill_perc = rx_bytes / master.um232_buff_sz * 100
-            master.um232_buff_wdgt.setValue(fill_perc)
-            if fill_perc > 90:
-                logging.warning("UM232H buffer might be overfilling")
+            # AI
+            if not self._app.devices.scanners.error_dict:
+                self._app.devices.scanners.fill_ai_buffer()
+                if self._app.meas.type not in {"SFCSSolution", "SFCSImage"}:
+                    self._app.devices.scanners.dump_ai_buff_overflow()
 
-        finally:
-            await asyncio.sleep(master.updt_intrvl["gui"])
+            #            tic = time.perf_counter() # TESTING
 
+            await asyncio.sleep(TIMEOUT)
 
-async def update_avg_counts(master) -> None:
-    """
-    Read new counts, and dump buffer overflow.
-    if update ready also average and show in GUI.
+    async def _update_gui(self) -> None:
+        """Doc."""
 
-    """
+        while self.not_finished:
 
-    while master.not_finished:
+            # scanners
+            updt_scn_pos(self._app)
 
-        try:
-            master.cntr_dvc.average_counts()
+            # Measurement progress bar
+            if self._app.meas.is_running:
+                updt_meas_progbar(self._app.meas)
 
-        except DeviceError:
-            pass
+            await asyncio.sleep(self.updt_intrvl["gui"])
 
-        else:
-            master._app.gui.main.counts.setValue(master.cntr_dvc.avg_cnt_rate)
+    async def _updt_um232h_status(self):
+        """Doc."""
 
-        finally:
-            await asyncio.sleep(master.updt_intrvl["cntr_avg"])
+        while self.not_finished:
 
+            try:
+                rx_bytes = self._app.devices.UM232H.get_status()
 
-async def update_dep(master) -> None:
-    """Update depletion laser GUI"""
+            except DeviceError:
+                pass
 
-    while master.not_finished:
-
-        try:
-            temp, pow, curr = (
-                master.dep_dvc.get_prop("temp"),
-                master.dep_dvc.get_prop("pow"),
-                master.dep_dvc.get_prop("curr"),
-            )
-
-        except DeviceError:
-            # do nothing if error in depletion laser
-            pass
-
-        else:
-            master.main_gui.depTemp.setValue(temp)
-            master.main_gui.depActualPow.setValue(pow)
-            master.main_gui.depActualCurr.setValue(curr)
-
-            if temp < master.dep_dvc.min_SHG_temp:
-                master.main_gui.depTemp.setStyleSheet("background-color: red; color: white;")
             else:
-                master.main_gui.depTemp.setStyleSheet("background-color: white; color: black;")
+                fill_perc = rx_bytes / self.um232_buff_sz * 100
+                self.um232_buff_wdgt.setValue(fill_perc)
+                if fill_perc > 90:
+                    logging.warning("UM232H buffer might be overfilling")
 
-        finally:
-            await asyncio.sleep(master.updt_intrvl["dep"])
+            finally:
+                await asyncio.sleep(self.updt_intrvl["gui"])
+
+    async def _update_avg_counts(self) -> None:
+        """
+        Read new counts, and dump buffer overflow.
+        if update ready also average and show in GUI.
+
+        """
+
+        while self.not_finished:
+
+            try:
+                self.cntr_dvc.average_counts()
+
+            except DeviceError:
+                pass
+
+            else:
+                self._app.gui.main.counts.setValue(self.cntr_dvc.avg_cnt_rate)
+
+            finally:
+                await asyncio.sleep(self.updt_intrvl["cntr_avg"])
+
+    async def _update_dep(self) -> None:
+        """Update depletion laser GUI"""
+
+        while self.not_finished:
+
+            try:
+                temp, pow, curr = (
+                    self.dep_dvc.get_prop("temp"),
+                    self.dep_dvc.get_prop("pow"),
+                    self.dep_dvc.get_prop("curr"),
+                )
+
+            except DeviceError:
+                # do nothing if error in depletion laser
+                pass
+
+            else:
+                self.main_gui.depTemp.setValue(temp)
+                self.main_gui.depActualPow.setValue(pow)
+                self.main_gui.depActualCurr.setValue(curr)
+
+                if temp < self.dep_dvc.min_SHG_temp:
+                    self.main_gui.depTemp.setStyleSheet("background-color: red; color: white;")
+                else:
+                    self.main_gui.depTemp.setStyleSheet("background-color: white; color: black;")
+
+            finally:
+                await asyncio.sleep(self.updt_intrvl["dep"])
+
+
+def get_last_line(file_path) -> str:
+    """
+    Return the last line of a text file.
+    (https://stackoverflow.com/questions/46258499/read-the-last-line-of-a-file-in-python)
+    """
+
+    try:
+        with open(file_path, "rb") as f:
+            f.seek(-2, os.SEEK_END)
+            while f.read(1) != b"\n":
+                f.seek(-2, os.SEEK_CUR)
+            last_line = f.readline().decode()
+    except FileNotFoundError:
+        return "Log File Not Found!!!"
+    else:
+        return last_line
+
+
+def updt_scn_pos(app):
+    """Doc."""
+
+    try:
+        (
+            x_ai,
+            y_ai,
+            z_ai,
+            x_ao_int,
+            y_ao_int,
+            z_ao_int,
+        ) = app.devices.scanners.ai_buffer[:, -1]
+
+    except IndexError:
+        # AI buffer has just been initialized
+        pass
+
+    else:
+        (x_um, y_um, z_um) = tuple(
+            (axis_vltg - axis_org) * axis_ratio
+            for axis_vltg, axis_ratio, axis_org in zip(
+                (x_ao_int, y_ao_int, z_ao_int),
+                app.devices.scanners.um_v_ratio,
+                app.devices.scanners.ORIGIN,
+            )
+        )
+
+        app.gui.main.xAIV.setValue(x_ai)
+        app.gui.main.yAIV.setValue(y_ai)
+        app.gui.main.zAIV.setValue(z_ai)
+
+        app.gui.main.xAOVint.setValue(x_ao_int)
+        app.gui.main.yAOVint.setValue(y_ao_int)
+        app.gui.main.zAOVint.setValue(z_ao_int)
+
+        app.gui.main.xAOum.setValue(x_um)
+        app.gui.main.yAOum.setValue(y_um)
+        app.gui.main.zAOum.setValue(z_um)
+
+
+def updt_meas_progbar(meas) -> None:
+    """Doc."""
+
+    try:
+        if meas.type == "SFCSSolution":
+            if not meas.cal:
+                progress = (
+                    (meas.total_time_passed + meas.time_passed)
+                    / (meas.total_duration * meas.duration_multiplier)
+                    * meas.prog_bar_wdgt.obj.maximum()
+                )
+            else:
+                progress = 0
+        elif meas.type == "SFCSImage":
+            progress = meas.time_passed / meas.est_total_duration * meas.prog_bar_wdgt.obj.maximum()
+        meas.prog_bar_wdgt.set(progress)
+
+    except AttributeError:
+        # happens when depletion is turned on before beginning measurement (5 s wait)
+        pass
+
+    except Exception as exc:
+        err_hndlr(exc, locals(), sys._getframe())
