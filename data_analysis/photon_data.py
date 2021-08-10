d8c4a4d3c23f07e6f354368cc41c594c7721e8d2
Ido Michealovich <40335982+Th3Tr00p3r@users.noreply.github.com>
2021-08-10 10:32:50 +0300

    Solved issue in scanning multiple planes - restarting CI task resets the internal DAQ buffer i guess, so I added a flag to avoid restarting the AI/CI tasks during image scans (only once before starting).

--- data_analysis/photon_data.py
+++ data_analysis/photon_data.py
@@ -2,7 +2,6 @@
 
 from dataclasses import dataclass
 
-import numba as nb
 import numpy as np
 
 import utilities.helper as helper
@@ -80,8 +79,22 @@ class PhotonData:
 
         self.runtime = counter
 
+    # TODO: move this to a more fitting module
     def convert_counts_to_images(self, counts, ao, scan_params: dict, um_v_ratio) -> None:
         """Doc."""
+
+        def calc_plane_image_stack(counts_stack, eff_idxs, pxls_per_line):
+            """Doc."""
+
+            n_lines, _, n_planes = counts_stack.shape
+            image_stack = np.empty((n_lines, pxls_per_line, n_planes))
+            norm_stack = np.empty((n_lines, pxls_per_line, n_planes))
+
+            for i in range(pxls_per_line):
+                image_stack[:, i, :] = counts_stack[:, eff_idxs == i, :].sum(axis=1)
+                norm_stack[:, i, :] = counts_stack[:, eff_idxs == i, :].shape[1]
+
+            return image_stack, norm_stack
 
         n_planes = scan_params["n_planes"]
         n_lines = scan_params["n_lines"]
@@ -106,21 +119,21 @@ class PhotonData:
         pxl_size_v = pxl_size_um / um_per_v
         pxls_per_line = helper.div_ceil(scan_params["dim1_lines_um"], pxl_size_um)
 
-        # remove counts from outside limits
+        # prepare to remove counts from outside limits
         dim1_ao_single = ao[0][:ppl]
         eff_idxs = ((dim1_ao_single - dim1_min) // pxl_size_v + 1).astype(np.int32)
-
         eff_idxs_forward = eff_idxs[:turn_idx]
         eff_idxs_backward = eff_idxs[-1 : (turn_idx - 1) : -1]
 
+        # create counts stack shaped (n_lines, ppl, n_planes) - e.g. 80 x 1000 x 1
         j0 = ppp * np.arange(n_planes)[:, np.newaxis]
         J = np.tile(np.arange(ppp), (n_planes, 1)) + j0
         counts_stack = np.diff(np.concatenate((j0, counts[J]), axis=1))
         counts_stack = counts_stack.T.reshape(n_lines, ppl, n_planes)
-
         counts_stack_forward = counts_stack[:, :turn_idx, :]
         counts_stack_backward = counts_stack[:, -1 : (turn_idx - 1) : -1, :]
 
+        # calculate the images and normalization separately for the forward/backward parts of the scan
         image_stack_forward, norm_stack_forward = calc_plane_image_stack(
             counts_stack_forward, eff_idxs_forward, pxls_per_line
         )
@@ -139,22 +152,6 @@ class PhotonData:
             line_scale_v,
             row_scale_v,
         )
-
-
-@nb.njit(cache=True)
-def calc_plane_image_stack(counts_stack, eff_idxs, pxls_per_line):
-    """Doc."""
-
-    n_lines, _, n_planes = counts_stack.shape
-    image_stack = np.empty((n_lines, pxls_per_line, n_planes))
-    norm_stack = np.empty((n_lines, pxls_per_line, n_planes))
-
-    for i in range(pxls_per_line):
-        matching_counts_stack = counts_stack[:, eff_idxs == i, :]
-        image_stack[i, :, :] = matching_counts_stack.sum(axis=1)
-        norm_stack[i, :, :] = matching_counts_stack.shape[1]
-
-    return image_stack.transpose(1, 0, 2), norm_stack.transpose(1, 0, 2)
 
 
 def find_section_edge(data, group_len, verbose=False):  # noqa c901
@@ -278,6 +275,6 @@ class ImageData:
     image_forward: np.ndarray
     norm_forward: np.ndarray
     image_backward: np.ndarray
-    normbackward: np.ndarray
+    norm_backward: np.ndarray
     line_ticks_v: np.ndarray
     row_ticks_v: np.ndarray
