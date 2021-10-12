ca212d99cd8a6fb5d22bfd6a1e56589ddc95076e
Lab PC <None>
2021-10-12 15:13:01 +0300

    added 'tests' package and a first test module (for testing scan_patterns module) as an example.

---
+++ tests/unit/scan_patterns_test.py
@@ -0,0 +1,21 @@
+import unittest
+
+import numpy as np
+
+from logic import scan_patterns
+
+
+class TestScanPatterns(unittest.TestCase):
+    """Doc."""
+
+    def test_calc_ao(self):
+        """Doc."""
+
+        set_pnts_lines_odd = np.random.randn(173)
+        single_line_ao = np.random.randn(578)
+        result = scan_patterns.calc_ao(set_pnts_lines_odd, single_line_ao)
+        self.assertEqual(result.shape, (2, set_pnts_lines_odd.size * single_line_ao.size))
+
+
+if __name__ == "__main__":
+    unittest.main()
