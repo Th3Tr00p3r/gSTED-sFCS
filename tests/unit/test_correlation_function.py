520281b83cd54c1deacdc13ea3ceb0cbc2b635e3
Idomich <None>
2021-11-02 15:08:39 +0200

    revised CF accumulation again

---
+++ tests/unit/test_correlation_function.py
@@ -0,0 +1,21 @@
+# import unittest
+#
+# import numpy as np
+#
+# from data_analysis import correlation_fucntion as module
+#
+#
+# class TestCorrelationFunction(unittest.TestCase):
+#    """Doc."""
+#
+#    def test_calc_ao(self):
+#        """Doc."""
+#
+#        set_pnts_lines_odd = np.random.randn(173)
+#        single_line_ao = np.random.randn(578)
+#        result = scan_patterns.calc_ao(set_pnts_lines_odd, single_line_ao)
+#        self.assertEqual(result.shape, (2, set_pnts_lines_odd.size * single_line_ao.size))
+#
+#
+# if __name__ == "__main__":
+#    unittest.main()
