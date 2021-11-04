added data_to_disk rotation to read_fpga_data (works fine). fixed show_external_axis() in display.py using the code I changed with Oleg. Other minor changes I guess.

data_analysis/photon_data.py
@@ -24,7 +24,7 @@ class TDCPhotonData:
 
 
 class TDCPhotonDataMixin:
-    """Doc."""
+    """Methods for creating and analyzing TDCPhotonData objects"""
 
     def convert_fpga_data_to_photons(
         self,
@@ -593,10 +593,10 @@ class CountsImageMixin:
         counts_stack_backward = counts_stack[:, -1 : (turn_idx - 1) : -1, :]
 
         # calculate the images and normalization separately for the forward/backward parts of the scan
-        image_stack_forward, norm_stack_forward = _calculate_plane_image_stack(
+        image_stack_forward, norm_stack_forward = self._calculate_plane_image_stack(
             counts_stack_forward, eff_idxs_forward, pxls_per_line
         )
-        image_stack_backward, norm_stack_backward = _calculate_plane_image_stack(
+        image_stack_backward, norm_stack_backward = self._calculate_plane_image_stack(
             counts_stack_backward, eff_idxs_backward, pxls_per_line
         )
 
@@ -612,19 +612,18 @@ class CountsImageMixin:
             plane_orientation=plane_orientation,
         )
 
+    def _calculate_plane_image_stack(self, counts_stack, eff_idxs, pxls_per_line):
+        """Doc."""
 
-def _calculate_plane_image_stack(counts_stack, eff_idxs, pxls_per_line):
-    """Doc."""
+        n_lines, _, n_planes = counts_stack.shape
+        image_stack = np.empty((n_lines, pxls_per_line, n_planes), dtype=np.int32)
+        norm_stack = np.empty((n_lines, pxls_per_line, n_planes), dtype=np.int32)
 
-    n_lines, _, n_planes = counts_stack.shape
-    image_stack = np.empty((n_lines, pxls_per_line, n_planes), dtype=np.int32)
-    norm_stack = np.empty((n_lines, pxls_per_line, n_planes), dtype=np.int32)
+        for i in range(pxls_per_line):
+            image_stack[:, i, :] = counts_stack[:, eff_idxs == i, :].sum(axis=1)
+            norm_stack[:, i, :] = counts_stack[:, eff_idxs == i, :].shape[1]
 
-    for i in range(pxls_per_line):
-        image_stack[:, i, :] = counts_stack[:, eff_idxs == i, :].sum(axis=1)
-        norm_stack[:, i, :] = counts_stack[:, eff_idxs == i, :].shape[1]
-
-    return image_stack, norm_stack
+        return image_stack, norm_stack
 
 
 def _find_section_edges(byte_data, group_len):  # NOQA C901
@@ -633,8 +632,7 @@ def _find_section_edges(byte_data, group_len):  # NOQA C901
     """
 
     # find index of first complete photon (where 248 and 254 bytes are spaced exatly (group_len -1) bytes apart)
-    edge_start = _first_full_photon_idx(byte_data, group_len)
-    if edge_start is None:
+    if (edge_start := _first_full_photon_idx(byte_data, group_len)) is None:
         raise RuntimeError("No byte data found! Check detector and FPGA.")
 
     # slice byte_data where assumed to be 248 and 254 (photon brackets)

utilities/display.py
@@ -137,6 +137,67 @@ class Display:
             self.canvas.draw_idle()
 
 
+@contextmanager
+def show_external_axes(
+    fig=None,
+    subplots=(1, 1),
+    figsize=None,
+    super_title=None,
+    should_force_aspect=False,
+    fontsize=14,
+    xlim: Tuple[float, float] = None,
+    ylim: Tuple[float, float] = None,
+    should_autoscale=False,
+):
+    """
+    Creates or accepts a Matplotlib figure, and yields a 'matplotlib.axes.Axes' object
+    which is to be manipulated, then shows the figure.
+    """
+
+    if fig is None:
+        fig = plt.figure(figsize=figsize)
+        axes = fig.subplots(*subplots)
+        if not isinstance(axes, Iterable):
+            axes = np.array([axes])
+    else:
+        axes = np.array(fig.get_axes())
+
+    try:
+        if axes.size == 1:
+            yield axes[0]
+        else:
+            yield axes
+
+    finally:
+        for ax in axes.flatten().tolist():
+            if should_force_aspect:
+                force_aspect(ax, aspect=1)
+            if xlim is None and should_autoscale:
+                ax.autoscale(enable=True, axis="x")
+            else:
+                ax.set_xlim(xlim)
+            if ylim is None and should_autoscale:
+                ax.autoscale(enable=True, axis="y")
+            else:
+                ax.set_ylim(ylim)
+
+            [item.set_fontsize(fontsize) for item in [ax.title, ax.xaxis.label, ax.yaxis.label]]
+
+        if super_title is not None:
+            fig.suptitle(super_title, fontsize=(fontsize + 2))
+
+        fig.show()
+
+
+def get_fig_with_axes(subplots=(1, 1), figsize: Tuple[float, float] = None):
+    """Doc."""
+
+    fig = plt.figure(figsize=figsize)
+    axes = fig.subplots(*subplots)
+
+    return fig, axes
+
+
 class NavigationToolbar(NavigationToolbar2QT):
     """
     Only display the buttons we need.
@@ -272,43 +333,3 @@ def force_aspect(ax, aspect=1) -> None:
     img, *_ = ax.get_images()
     extent = img.get_extent()
     ax.set_aspect(abs((extent[1] - extent[0]) / (extent[3] - extent[2])) / aspect)
-
-
-@contextmanager
-def show_external_axes(
-    fig=None, subplots=(1, 1), super_title=None, should_force_aspect=False, fontsize=14
-):
-    """
-    Creates or accepts a Matplotlib figure, and yields a 'matplotlib.axes.Axes' object
-    which is to be manipulated, then shows the figure.
-    """
-
-    if fig is None:
-        fig = plt.figure()
-        axes = fig.subplots(*subplots)
-    else:
-        axes = fig.get_axes()
-
-    try:
-        yield axes
-
-    finally:
-        if not isinstance(axes, Iterable):
-            axes = np.array([axes])
-        for ax in axes.ravel():
-            if should_force_aspect:
-                force_aspect(ax, aspect=1)
-            ax.autoscale()
-            [item.set_fontsize(fontsize) for item in [ax.title, ax.xaxis.label, ax.yaxis.label]]
-        if super_title is not None:
-            fig.suptitle(super_title, fontsize=(fontsize + 2))
-        fig.show()
-
-
-def get_fig_with_axes(subplots=(1, 1)):
-    """Doc."""
-
-    fig = plt.figure()
-    axes = fig.subplots(*subplots)
-
-    return fig, axes