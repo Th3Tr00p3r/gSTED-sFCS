26335719503991bf89e56e6f2dad22414f403ac7
Ido Michealovich <40335982+Th3Tr00p3r@users.noreply.github.com>
2022-03-23 14:24:59 +0200

    Added pattern display in solution measurement analysis. Adding epsilon=1 to variance in row score calculation to avoid zero division error when analyzing/testing very short/no laser measurements

--- gui/gui.py
+++ gui/gui.py
@@ -30,6 +30,7 @@ class MainWin(QtWidgets.QMainWindow):
         self.solScanAcf = GuiDisplay(self.solScanAcfLayout, self)
         self.imgScanPattern = GuiDisplay(self.imgScanPatternLayout)
         self.solScanPattern = GuiDisplay(self.solScanPatternLayout)
+        self.solScanAnalysisPattern = GuiDisplay(self.solAnalysisScanTestLayout)
         self.solExpLoading = GuiDisplay(self.solExpLoadingLayout, self)
         self.solExpTDC1 = GuiDisplay(self.solAnalysisTDCLayout1, self)
         self.solExpTDC2 = GuiDisplay(self.solAnalysisTDCLayout2, self)
