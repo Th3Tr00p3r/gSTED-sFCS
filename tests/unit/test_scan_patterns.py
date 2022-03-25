c4db1defb2e54ba42afa3c8f595cf2b4a6253556
Ido Michealovich <40335982+Th3Tr00p3r@users.noreply.github.com>
2022-03-26 01:27:09 +0300

    attempt to fix circular scan plus refactoring and name changes across all scan-pattern related stuff

--- tests/unit/test_scan_patterns.py
+++ tests/unit/test_scan_patterns.py
@@ -13,7 +13,7 @@ class TestScanPatterns(unittest.TestCase):
 
         set_pnts_lines_odd = np.random.randn(173)
         single_line_ao = np.random.randn(578)
-        result = scan_patterns.calc_ao(set_pnts_lines_odd, single_line_ao)
+        result = scan_patterns.calculate_image_ao(set_pnts_lines_odd, single_line_ao)
         self.assertEqual(result.shape, (2, set_pnts_lines_odd.size * single_line_ao.size))
 
 
