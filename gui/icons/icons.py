56ac7cc36a540806170c795bbccee7de7cd76eaa
Ido Michealovich <40335982+Th3Tr00p3r@users.noreply.github.com>
2022-09-28 14:26:21 +0300

    WIP - icons refactoring

--- gui/icons/icons.py
+++ gui/icons/icons.py
@@ -5,7 +5,7 @@ from PyQt5.QtGui import QIcon
 ICONS_PATH = Path("./gui/icons")
 
 
-def gen_icon_paths_dict(dir_path: Path = ICONS_PATH, filetype="png"):
+def get_icon_paths(dir_path: Path = ICONS_PATH, filetype="png"):
     """Doc."""
 
     icon_paths_dict = {}
@@ -14,10 +14,5 @@ def gen_icon_paths_dict(dir_path: Path = ICONS_PATH, filetype="png"):
         *_, icon_fname = Path(icon_path).parts
         icon_fname_no_extension = Path(icon_fname).stem
         icon_paths_dict[icon_fname_no_extension] = f"{dir_path}/{icon_fname}"
-    return icon_paths_dict
 
-
-def get_icon_paths():
-    """Doc."""
-
-    return {key: QIcon(val) for key, val in gen_icon_paths_dict().items()}
+    return {key: QIcon(val) for key, val in icon_paths_dict.items()}
