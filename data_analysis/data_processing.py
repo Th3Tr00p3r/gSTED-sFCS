Fixed major issue with afterpulsing filtering - now normalizing to gate window for TDC gates as well as detector ones. WIP - not finished with plotting issues. Other minor changes.

data_analysis/data_processing.py
@@ -12,7 +12,7 @@ import scipy
 import skimage
 
 from utilities.display import Plotter
-from utilities.fit_tools import FIT_NAME_DICT, FitParams, curve_fit_lims
+from utilities.fit_tools import FIT_NAME_DICT, FitError, FitParams, curve_fit_lims
 from utilities.helper import Gate, Limits, div_ceil, nan_helper, xcorr
 
 
@@ -412,8 +412,10 @@ class AfterulsingFilter:
     """Doc."""
 
     t_hist: np.ndarray
+    all_hist_norm: np.ndarray
     baseline: float
     I_j: np.ndarray
+    norm_factor: float
     M: np.ndarray
     filter: np.ndarray
 
@@ -429,9 +431,14 @@ class AfterulsingFilter:
         ) as axes:
             axes[0].set_title("Filter Ingredients")
             axes[0].set_yscale("log")
-            axes[0].plot(self.t_hist[:n], self.I_j, label="I_j (raw histogram)")
             axes[0].plot(
-                self.t_hist[:n], self.baseline * np.ones(self.t_hist[:n].shape), label="baseline"
+                self.t_hist[:n], self.all_hist_norm / self.norm_factor, label="norm. raw histogram"
+            )
+            axes[0].plot(self.t_hist[:n], self.I_j / self.norm_factor, label="norm. I_j (fit)")
+            axes[0].plot(
+                self.t_hist[:n],
+                self.baseline / self.norm_factor * np.ones(self.t_hist[:n].shape),
+                label="norm. baseline",
             )
             axes[0].plot(
                 self.t_hist[:n], self.M.T[0], label="M_j1 (ideal fluorescence decay curve)"
@@ -440,10 +447,9 @@ class AfterulsingFilter:
                 self.t_hist[:n], self.M.T[1], label="M_j2 (ideal afterpulsing 'decay' curve)"
             )
             axes[0].legend()
-            axes[0].set_ylim(self.baseline / 10, None)
+            axes[0].set_ylim(self.baseline / self.norm_factor / 10, None)
 
             axes[1].set_title("Filter")
-            axes[1].set_ylim(-1, 2)
             axes[1].plot(self.t_hist, self.filter.T)
             axes[1].plot(self.t_hist, self.filter.sum(axis=0))
             axes[1].legend(["F_1j (signal)", "F_2j (afterpulsing)", "F.sum(axis=0)"])
@@ -538,7 +544,7 @@ class TDCCalibration:
 
     def calculate_afterpulsing_filter(
         self,
-        detector_gate_ns,
+        gate_ns,
         should_plot=False,
         **kwargs,
     ) -> np.ndarray:
@@ -548,54 +554,65 @@ class TDCCalibration:
         all_hist_norm = copy(self.all_hist_norm)
         t_hist = copy(self.t_hist)
 
-        # crop-out the detector-gated part so it does not interfere wth filter creation
-        if detector_gate_ns.lower > 0:
-            peak_idx = round(detector_gate_ns.lower * 10)  # x step is 0.1 ns
-            all_hist_norm = np.roll(all_hist_norm, -peak_idx)[:-peak_idx]
-            t_hist = np.roll(t_hist, -peak_idx)[:-peak_idx]
-            F_gated = np.full((2, peak_idx), np.nan)
+        # crop-out the gated part of the histogram
+        if gate_ns:
+            in_gate_idxs = gate_ns.valid_indices(t_hist)
+            t_hist = t_hist[in_gate_idxs]
+            all_hist_norm = all_hist_norm[in_gate_idxs]
+            lower_idx = round(gate_ns.lower * 10)
+            F_pad_before = np.full((2, lower_idx), np.nan)
+            try:
+                upper_idx = round(gate_ns.upper * 10)
+                F_pad_after = np.full((2, len(self.t_hist) - upper_idx - 1), np.nan)
+            except (OverflowError, ValueError):
+                # OverflowError: gate_ns.upper == np.inf
+                # ValueError: len(self.t_hist) == upper_idx
+                F_pad_after = np.array([[], []])
         else:
-            peak_idx = 0
+            lower_idx = 0
 
         # interpolate over NaNs
         nans, x = nan_helper(all_hist_norm)  # get nans and a way to interpolate over them later
         #        print("nans: ", nans.sum()) # TESTESTEST - why always 23?
         all_hist_norm[nans] = np.interp(x(nans), x(~nans), all_hist_norm[~nans])
 
-        # normalize
-        all_hist_norm = all_hist_norm / all_hist_norm.sum()
-
         # Use exponential fit to get the underlying exponentioal decay and background (Smoothing is what's really essential here)
         # TODO: for STED measurement I will need a different fit - decay is no longer purely exponential
-        fp = curve_fit_lims(
-            (fit_func := FIT_NAME_DICT["exponent_with_background_fit"]),
-            [1e-2, 4, 1e-4],
-            xs=t_hist,
-            ys=all_hist_norm,
-            x_limits=Limits(peak_idx / 10 + 2.5, np.inf),
-            plot_kwargs=dict(y_scale="log"),
-        )
+        fit_lower_x_limit = t_hist[np.argmax(all_hist_norm)] + 2
+        try:
+            fp = curve_fit_lims(
+                (fit_func := FIT_NAME_DICT["exponent_with_background_fit"]),
+                [1e-2, 4, 1e-4],
+                xs=t_hist,
+                ys=all_hist_norm,
+                x_limits=Limits(fit_lower_x_limit, np.inf),
+                plot_kwargs=dict(y_scale="log"),
+            )
+        except FitError as exc:
+            raise ValueError(f"Gate {gate_ns} is too narrow! [{exc}]")
+
         baseline = fp.beta["bg"]
-        all_hist_norm = fit_func(t_hist.astype(np.float64), *fp.beta.values())
+        fitted_all_hist_norm = fit_func(t_hist.astype(np.float64), *fp.beta.values())
+
+        # normalization factor
+        norm_factor = (fitted_all_hist_norm - baseline).sum()
 
         # define matrices and calculate F
-        M_j1 = all_hist_norm - baseline  # p1
+        M_j1 = (fitted_all_hist_norm - baseline) / norm_factor  # p1
         M_j2 = 1 / len(t_hist) * np.ones(t_hist.shape)  # p2
         M = np.vstack((M_j1, M_j2)).T
 
-        I_j = all_hist_norm
+        I_j = fitted_all_hist_norm  # / norm_factor
         I = np.diag(I_j)  # NOQA E741
         inv_I = np.linalg.pinv(I)
 
         F = np.linalg.pinv(M.T @ inv_I @ M) @ M.T @ inv_I
 
         # Return the filter to original dimensions by adding zeros in the detector-gated zone
-        if detector_gate_ns.lower > 0:
-            F = np.hstack((F_gated, F))
-        else:
-            F = F
+        if gate_ns:
+            F = np.hstack((F_pad_before, F, F_pad_after))
 
-        ap_filter = AfterulsingFilter(self.t_hist, baseline, I_j, M, F)
+        ap_filter = AfterulsingFilter(self.t_hist, all_hist_norm, baseline, I_j, norm_factor, M, F)
 
         if should_plot:
             ap_filter.plot()

data_analysis/example_scripts/Testing Detector Gating.py
@@ -7,7 +7,7 @@
 #       format_version: '1.3'
 #       jupytext_version: 1.13.8
 #   kernelspec:
-#     display_name: Python 3
+#     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
@@ -78,8 +78,8 @@ AP_METHOD = "filter"
 
 NORM_RANGE = (7e-3, 9e-3)
 
-# FILES = "Use 1"
-FILES = "Use 1-5"
+FILES = "Use 1"
+# FILES = "Use 1-5"
 # FILES = "Use All"
 
 data_label_kwargs = {
@@ -158,20 +158,9 @@ print(data_label_kwargs)
 # Importing all needed data. Processing, correlating and averaging if no pre-processed measurement exist.
 
 # %%
-SHOULD_USE_FIRST_AP_FILTER = False
-
 # load experiment
 ap_filter_list = []
 for label, exp in exp_dict.items():
-
-    # use external filter if available
-    try:
-        if SHOULD_USE_FIRST_AP_FILTER:
-            ext_filter = ap_filter_list[0]
-        else:
-            ext_filter = None
-    except IndexError:
-        ext_filter = None
 
     # skip already loaded experiments, unless forced
     if not hasattr(exp_dict[label], "confocal") or data_label_kwargs[label]["force_processing"]:
@@ -179,20 +168,13 @@ for label, exp in exp_dict.items():
             should_plot=False,
             should_re_correlate=FORCE_ALL,
             **data_label_kwargs[label],
-            external_ap_filter=ext_filter,
         )
-
-        #         # calibrate TDC # TESTESTEST
-        #         exp.confocal.calibrate_tdc(**data_label_kwargs[label])
 
         # plot TDC calibration
         try:
             exp.confocal.tdc_calib.plot()
         except AttributeError:
             print("NO TDC CALIBRATION TO PLOT!")
-
-        # keep signal filters
-        ap_filter_list.append(exp.confocal._afterpulsing_filter)
 
 #         # save processed data (to avoid re-processing)
 #         exp.save_processed_measurements(
@@ -270,51 +252,25 @@ for label, exp in exp_dict.items():
         print("NO TDC CALIBRATION!")
 
 # %% [markdown]
-# ## Comparing the filters calculated for free-running measurements to those of 0 ns in gated mode
-
-# %%
-with Plotter(ylim=(-2, 2)) as ax:
-    for label, exp in exp_dict.items():
-        t_hist = exp.confocal.tdc_calib.t_hist
-        ap_filter = exp.confocal._afterpulsing_filter.filter[0]
-        ax.plot(t_hist, ap_filter, label=label)
-
-    ax.legend()
-
-
-# %% [markdown]
 # ## Testing the effect of TDC gating on confocal measurements
 
 # %% [markdown]
 # Gating and calculating the gated filtered afterpulsings
 
 # %%
-# CHOOSE GATES
-# UPPER_GATES = [30, np.inf]
-UPPER_GATES = [20, 40, 60, 80, np.inf]
+from itertools import product
 
-SHOULD_CALCULATE_AP = False
-# SHOULD_CALCULATE_AP = True
+lower_gate_list = [7.5]
+upper_gate_list = [20, 60, np.inf]
 
-# GATE AND CALCULATE GATED APs
-tdc_gates_ns = [(7.5, upper_gate) for upper_gate in UPPER_GATES]
+gate_list = [gate_ns for gate_ns in product(lower_gate_list, upper_gate_list)]
+
 for label, exp in exp_dict.items():
     if FORCE_ALL:
-        try:
-            # CALCULATE UNGATED AP
-            if SHOULD_CALCULATE_AP:
-                exp.confocal.calculate_filtered_afterpulsing()
-
-            # GATE & CALCULATE GATED APs
-            for tdc_gate_ns in tdc_gates_ns:
-                exp.add_gate(
-                    tdc_gate_ns, meas_type="confocal", should_plot=False, **data_label_kwargs[label]
-                )
-                if SHOULD_CALCULATE_AP:
-                    exp.confocal.calculate_filtered_afterpulsing(tdc_gate_ns=tdc_gate_ns)
-
-        except AttributeError:
-            print("NO TDC CALIBRATION!")
+        # GATE
+        exp.add_gates(
+            gate_list, meas_type="confocal", should_plot=False, **data_label_kwargs[label]
+        )
 
     #         else:
     #             # save processed data (to avoid re-processing)