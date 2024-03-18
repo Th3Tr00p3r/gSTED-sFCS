1cb617c77beed88c9780d5e1a033dad0a8d20f8d
Lab PC <40335982+Th3Tr00p3r@users.noreply.github.com>
2024-03-18 11:08:22 +0200

    testing NaN in DBScan issue

--- utilities/helper.py
+++ utilities/helper.py
@@ -564,7 +564,7 @@ def dbscan_noise_thresholding(
         nan_per_row = np.isnan(X).sum(axis=1)  # TESTESTEST
         print(f"Warning: {(nan_per_row>0).sum()}/{X.shape[0]} ACFs contain NaNs!")  # TESTESTEST
         print(
-            f"The bad rows contain {', '.join([nans for nans in nan_per_row])}NaNs."
+            f"The bad rows contain {', '.join([str(nans) for nans in nan_per_row])}NaNs."
         )  # TESTESTEST
 
     # Standardize the data
