8ade3664e521678f81f3cbdc1452a948eb9e1fd9
Lab PC <40335982+Th3Tr00p3r@users.noreply.github.com>
2024-03-31 11:59:50 +0300

    Fixes previous commit

--- data_analysis/correlation_function.py
+++ data_analysis/correlation_function.py
@@ -933,7 +933,7 @@ class SolutionSFCSMeasurement:
         if (
             should_parallel_process
             and ((n_files := len(file_paths)) >= 5)
-            and (total_byte_data_size_estimate_mb > 500)
+            #            and (total_byte_data_size_estimate_mb > 500)
         ):
             # -2 is one CPU left to OS and one for I/O
             N_RAM_PROCESSES = N_CPU_CORES - 2
@@ -998,7 +998,7 @@ class SolutionSFCSMeasurement:
                     self.data.append(p)
 
             # sort the data by file number
-            self.data = sorted(self.data)
+            self.data.sort()
 
             print("\nMultiprocessing complete!")
 
